9101f1b142654c02b8a6aa245d43a3a2ac570399
deveshmanjhi <devesh.manjhi.cse14@itbhu.ac.in>
2015-03-13 19:09:48 +0530

    this is simpson method earlier was trapezoidal

--- Definite integral.py
+++ Definite integral.py
@@ -14,6 +14,17 @@ class Integrate():
     h=float(b-a)/n
     return s*h/2.0
     
+   def simpson(self,f,a,b,n):
+        self.x=numpy.linspace(a,b,n+1)
+        y=f(self.x)
+    
+        h = float(self.x[1] - self.x[0])
+        n = len(self.x) - 1
+        if n % 2 == 1:
+            n -= 1
+        s = y[0] + y[n] + 4.0 * sum(y[1:-1:2]) + 2.0 * sum(y[2:-2:2])
+        return h * s / 3.0
+    
     
   
     
